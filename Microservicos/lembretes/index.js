1c93f2966ac37bebbfa5e2f284422e7ef882a8f1
Danilo-Miguel <danilo.miguel@cromai.com>
2022-09-19 11:30:30 -0300

    Feat: Integração dos barramentos

--- Microservicos/lembretes/index.js
+++ Microservicos/lembretes/index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+
+const axios = require("axios");
+
 const app = express();
 app.use(bodyParser.json())
 const lembretes = {};
@@ -10,15 +13,27 @@ app.get('/lembretes', (req, res) =>{
 
 });
 
-app.put('/lembretes', (req, res) => {
+app.put('/lembretes', async (req, res) => {
     contador++;
     const{texto} = req.body;
     lembretes[contador] = {
-        contador, texto
-    }
-    res.send(201).send(lembretes[contador])
-
+        contador, 
+        texto, 
+};
+await axios.post("http://localhost:10000/eventos", {
+    tipo: "LembreteCriado",
+    dados:{
+        contador,
+        texto,
+    },
 });
+res.status(201).send(lembretes[contador]);
+});
+
+app.post("/eventos", (req, res) => {
+    console.log(req.body);
+    res.status(200).send({msg: "ok"});
+})
 
 app.listen(4000, () =>{
 console.log('Microserviço de lembretes executando na porta 4000')
