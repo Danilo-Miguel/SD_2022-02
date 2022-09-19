346930b574464ad353ab7bbb83318f77b3b1f205
Danilo-Miguel <danilo.miguel@cromai.com>
2022-09-19 09:17:03 -0300

    Refactor: Integração entre o mocroserviço de obersavações e lembrete

--- Microservicos/lembretes/index.js
+++ Microservicos/lembretes/index.js
@@ -1,14 +1,25 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express();
+app.use(bodyParser.json())
 const lembretes = {};
+var contador = 0; 
+
 app.get('/lembretes', (req, res) =>{
+    res.send(lembretes);
 
 });
 
 app.put('/lembretes', (req, res) => {
+    contador++;
+    const{texto} = req.body;
+    lembretes[contador] = {
+        contador, texto
+    }
+    res.send(201).send(lembretes[contador])
 
 });
 
 app.listen(4000, () =>{
-console.log('Microserviço de lembrestes executando na porta 4000')
+console.log('Microserviço de lembretes executando na porta 4000')
 });
