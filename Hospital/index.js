c0dde84218fef05b1282312dde6c069f4e7b4131
Danilo-Miguel <danilo.miguel@cromai.com>
2022-10-13 11:14:52 -0300

    Inserção de médicos via código

--- Hospital/index.js
+++ Hospital/index.js
@@ -1,30 +1,48 @@
-const express = require ('express')
+const express = require('express')
 //mysql é o nome de uma variável, pode ser qualquer coisa
 //mysql parece mais intuitivo do que mysql2
 const mysql = require('mysql2')
 const app = express()
 app.use(express.json())
+
+app.post('/medicos', (req, res) => {
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'hospital',
+        password: 'password'
+    })
+    const crm = req.body.crm
+    const nome = req.body.nome
+    const sql = "INSERT INTO medico (crm, nome) VALUES (" + crm
+        + ", '" + nome + "')"
+    connection.query(sql, (err, results, fields) => {
+        console.log(results)
+        console.log(fields)
+        res.send('ok')
+    })
+})
 app.get('/medicos', (req, res) => {
-const connection = mysql.createConnection({
-host: 'localhost',
-user: 'root',
-database: 'hospital',
-password: 'password'
-})
-connection.query('SELECT * FROM medico', (err, results, fields) => {
-res.json(results)
-})
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'hospital',
+        password: 'password'
+    })
+    connection.query('SELECT * FROM medico', (err, results, fields) => {
+        res.json(results)
+    })
 })
 app.get('/pacientes', (req, res) => {
-const connection = mysql.createConnection({
-host: 'localhost',
-user: 'root',
-database: 'hospital',
-password: 'password'
-})
-connection.query('SELECT * FROM paciente', (err, results, fields) => {
-res.json(results)
-})
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'hospital',
+        password: 'password'
+    })
+    connection.query('SELECT * FROM paciente', (err, results, fields) => {
+        res.json(results)
+    })
 })
 const porta = 3000
 app.listen(porta, () => console.log(`Executando. Porta ${porta}`))
